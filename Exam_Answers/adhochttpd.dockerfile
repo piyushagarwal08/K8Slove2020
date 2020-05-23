3bf4ad1e55851b20d7e8460973cd80948a06e6c8
Piyush Agarwal <piyushagarwal.0108@gmail.com>
2020-05-23 19:54:02 +0530

    changes

--- Exam_Answers/adhochttpd.dockerfile
+++ Exam_Answers/adhochttpd.dockerfile
@@ -2,9 +2,8 @@ FROM httpd
 MAINTAINER piyushagarwal.0108@gmail.com
 COPY webapp1 /temp/webapp1
 COPY webapp2 /temp/webapp2
-COPY start.sh ./
-WORKDIR ./
-RUN chmod +x start.sh
+COPY start.sh /temp/start.sh
+RUN chmod +x /temp/start.sh
 EXPOSE 80
 ENV x=app
-ENTRYPOINT ["/bin/bash","-c","./start.sh"]
+ENTRYPOINT ["/bin/bash","/temp/start.sh"]
--- Exam_Answers/start.sh
+++ Exam_Answers/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-if [ $x = "app1" ]
+if [ "$x" == "app1" ]
 then
-	cp /tmp/webapp1 /var/www/html/
-elif [ $x = "app2" ]
+	cp -rf /opt/webapp1/* /var/www/html/
+	httpd -DFOREGROUND
+elif [ "$x" == "app2" ]
 then
-	cp /tmp/webapp2 /var/www/html/
+	cp -rf /tmp/webapp2/* /var/www/html/
+	httpd -DFOREGROUND
 else
 	echo "Incorrect Environment Variable" > /var/www/html/index.html
+	httpd -DFOREGROUND
 fi
 
